09b98c56eb67e96fc5de98bd9f7091d6264ec2a2
RavandTaha <ravtah50399@stud.noroff.no>
2023-10-10 12:52:38 +0200

    added scripttutroial

--- scripttutorial.js
+++ scripttutorial.js
@@ -1,26 +1,32 @@
 // Define the URL of the external API
 const apiUrl = "https://api.noroff.dev/api/v1/gamehub";
 
+// Get the product-list container element
+const productList = document.getElementById("product-list");
 
-// Get the product list container element
-const productList = document.getElementById('product-list');
 
 // Fetch data from the API
 fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        // Process the data and create clickable links
-        data.forEach(product => {
-            const productItem = document.createElement('li');
-            const productLink = document.createElement('a');
-            productLink.href = "product.html";
+	.then((response) => response.json())
+	.then((data) => {
+		// Process the data and display it in the product-list container
+		data.forEach((product) => {
+			const productItem = document.createElement("div");
+			productItem.classList.add("product-item");
+            const productLink = document.createElement('a')
+            productLink.href = "product1.html";
             productLink.textContent = product.title;
             productItem.appendChild(productLink);
             productList.appendChild(productItem);
-        });
-    })
+			const description = document.createElement("p");
+			description.innerText = product.description;
+			productItem.append(description);
+			productList.append(productItem);
 
+		});
+	})
 	.catch((error) => {
-		products.innerHTML = '<div class="error">There was an error fetching the games</div>';
+		productList.innerHTML = '<div class="error">There was an error fetching the games</div>';
 		console.error("Error fetching data:", error);
 	});
+
