1128d2aefb96e2f044cc5db4dd23540277e0cf0b
RavandTaha <ravtah50399@stud.noroff.no>
2023-11-03 10:42:50 +0100

    added products in index

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
+// Define the URL of the external API
+const apiUrl = "https://api.noroff.dev/api/v1/gamehub";
+
+// Get the product-list container element
+const productList = document.getElementById("product-list");
+const productList2 = document.getElementById("product-list2");
 
 
 if (bar) {
@@ -15,4 +21,72 @@ if (close) {
     nav.classList.remove('active');
   })
 }
+
+
+// Fetch data from the API for homepage pages
+fetch(apiUrl)
+.then((response) => response.json())
+.then((data) => {
+  // Process the data and display it in the product-list container
+  data.forEach((product) => {
+    const productItem = document.createElement("div");
+    productItem.classList.add("product-item");
+          const productLink = document.createElement('a')
+          productLink.href = "product2.html";
+          productLink.textContent = product.title;
+          productItem.appendChild(productLink);
+          productList2.appendChild(productItem);
+    const image = document.createElement("img");
+    image.src = product.image;
+    productItem.append(image);
+    productList2.append(productItem);
+
+  });
+ })
+
+  // catch error
+  .catch((error) => {
+  productList2.innerHTML = '<div class="error">There was an error fetching the games</div>';
+  console.error("Error fetching data:", error);
+  });
+
+
+// Fetch data from the API for categories pages
+fetch(apiUrl)
+	.then((response) => response.json())
+	.then((data) => {
+		// Process the data and display it in the product-list container
+		data.forEach((product) => {
+			const productItem = document.createElement("div");
+			productItem.classList.add("product-item");
+            const productLink = document.createElement('a')
+            productLink.href = "product2.html";
+            productLink.textContent = product.title;
+            productItem.appendChild(productLink);
+            productList.appendChild(productItem);
+			const description = document.createElement("p");
+			description.innerText = product.description;
+			productItem.append(description);
+			const image = document.createElement("img");
+			image.src = product.image;
+			productItem.append(image);
+			productList.append(productItem);
+
+		});
+	})
+
+       // catch error
+	.catch((error) => {
+		productList.innerHTML = '<div class="error">There was an error fetching the games</div>';
+		console.error("Error fetching data:", error);
+	});
+
+
+
+ 
+
+
+
+
+
 
